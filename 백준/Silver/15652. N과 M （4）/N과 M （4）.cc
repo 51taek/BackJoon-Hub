72556026ed086fc65749b5c073fd22b6413b9a4e
wontaek oh <61085049+51taek@users.noreply.github.com>
2025-11-05 13:45:05 +0900

    [Silver III] Title: N과 M (4), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[10];
+
+void loop(int idx){
+
+  if(idx==m){
+    for(int i=0;i<m;i++){
+      cout << arr[i] << " ";
+    }
+    cout << "\n";
+    return ;
+  }
+
+  int st = 1;
+  if(idx != 0) st = arr[idx-1];
+
+  for(int i=st;i<=n;i++){
+    arr[idx] = i;
+    loop(idx+1);
+  }
+}
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);;
+  cout.tie(0);
+
+  cin >> n >> m;
+
+  loop(0);
+  
+}
