4109cd7193f7e784bff39848a5a1ce8ee69accd1
wontaek oh <61085049+51taek@users.noreply.github.com>
2025-11-18 10:53:46 +0900

    [Silver IV] Title: 상근이의 여행, Time: 16 ms, Memory: 2044 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t;
+vector<int> adj[1005];
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  cin >> t;
+  for(int i=0;i<t;i++){
+    int n, m;
+    cin >> n >> m;
+    while(m--){
+      int a, b;
+      cin >> a >> b;
+    }
+    cout << n-1 << "\n";
+    
+  }
+  
+}
