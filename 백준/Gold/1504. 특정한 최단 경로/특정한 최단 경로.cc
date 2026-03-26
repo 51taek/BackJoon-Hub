c4bed3172f247a386349a949b8c33b682fced2b7
wontaek oh <61085049+51taek@users.noreply.github.com>
2026-03-26 15:00:45 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 44 ms, Memory: 5748 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
@@ -1,61 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n, m;
+vector<pair<int,int>> adj[1000];
+long long d[1000];
+
 #define X first
 #define Y second
 
-int n, m;
-int a, b;
+const int INF = 10e9+10;
 
-const int INF = 0x3f3f3f3f;
-
-vector<pair<int,int>> graph[1000];
-int d[1000];
-
-long long mini(int st, int en){
-  priority_queue<pair<int,int>,
-    vector<pair<int,int>>,
-    greater<pair<int,int>> > pq;
-
-  fill(d, d + n + 1, INF);
+long long func(int st, int en){
   
+  priority_queue<pair<int,int>,
+  vector<pair<int,int>>,
+  greater<pair<int,int>> > pq;
+
+  fill(d,d+1000,INF);
+
   d[st] = 0;
-  pq.push({d[st],st});
+  pq.push({0,st});
+
   while(!pq.empty()){
     auto cur = pq.top(); pq.pop();
-    if(d[cur.Y] != cur.X) continue;
 
-    for(auto nxt: graph[cur.Y]){
+    if(d[cur.Y] != cur.X) continue;
+    for(auto nxt: adj[cur.Y]){
       if(d[nxt.Y] <= d[cur.Y] + nxt.X) continue;
       d[nxt.Y] = d[cur.Y] + nxt.X;
       pq.push({d[nxt.Y], nxt.Y});
     }
   }
+
   return d[en];
 }
 
 int main(){
+
   ios::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
 
   cin >> n >> m;
 
-
   for(int i=0;i<m;i++){
-    int x, y, cost;
-    cin >> x >> y >> cost;
-    graph[x].push_back({cost,y});
-    graph[y].push_back({cost,x});
+    int u, v, cost;
+    cin >> u  >> v >> cost;
+    adj[u].push_back({cost,v});
+    adj[v].push_back({cost,u});
   }
 
-  cin >> a >> b;
+  int p1, p2;
+  cin >> p1 >> p2;
 
-  long long ans1 = mini(1,a) + mini(a,b) + mini(b,n);
-  long long ans2 = mini(1,b) + mini(b,a) + mini(a,n);
+  long long ans1 = func(1,p1) + func(p1,p2) + func(p2,n);
+  long long ans2 = func(1,p2) + func(p2,p1) + func(p1,n);
 
+  
   long long ans = min(ans1,ans2);
-  if(ans >= INF) ans = -1;
-  cout << ans;
+  if(ans >= INF) cout << -1;
+  else cout << ans;
+
+  
   
 }
