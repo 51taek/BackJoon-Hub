bcaa7dde52c27d1b53f74ab5001840de7e33b97e
wontaek oh <61085049+51taek@users.noreply.github.com>
2026-03-27 13:25:18 +0900

    [Gold III] Title: 감시, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.cc
+++ 백준/Gold/15683. 감시/감시.cc
@@ -1,93 +1,101 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int board1[10][10];
-int board2[10][10];
-vector<pair<int,int>> cctv;
 int n, m;
+int a[10][10];
+int b[10][10];
 
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
+vector<pair<int,int>> camera;
 
-bool OOB(int a, int b){
-  return a < 0 || a >= n || b < 0 || b >= m;
-}
+#define X first
+#define Y second
 
+int mn = INT_MAX;
 
 void upd(int x, int y, int dir){
   dir %= 4;
   while(1){
     x += dx[dir];
     y += dy[dir];
-    if(OOB(x,y) || board2[x][y] == 6) return;
-    if(board2[x][y] != 0) continue;
-    board2[x][y] = 7;
+
+    if(x < 0 || y < 0 || x >= n || y >= m) return;
+    if(b[x][y] == 6) return;
+    if(b[x][y] != 0) continue;
+    b[x][y] = 7;
+  }
+}
+
+void dfs(int idx){
+  if(idx == camera.size()){
+    int val = 0;
+    for(int i = 0; i < n; i++)
+      for(int j = 0; j < m; j++)
+        val += (b[i][j] == 0);
+
+    mn = min(mn, val);
+    return;
+  }
+
+  int x = camera[idx].X;
+  int y = camera[idx].Y;
+  int type = a[x][y];
+
+  int backup[10][10];
+  for(int i = 0; i < n; i++)
+    for(int j = 0; j < m; j++)
+      backup[i][j] = b[i][j];
+
+  for(int dir = 0; dir < 4; dir++){
+    for(int i = 0; i < n; i++)
+      for(int j = 0; j < m; j++)
+        b[i][j] = backup[i][j];
+
+    if(type == 1){
+      upd(x, y, dir);
+    }
+    else if(type == 2){
+      upd(x, y, dir);
+      upd(x, y, dir + 2);
+    }
+    else if(type == 3){
+      upd(x, y, dir);
+      upd(x, y, dir + 1);
+    }
+    else if(type == 4){
+      upd(x, y, dir);
+      upd(x, y, dir + 1);
+      upd(x, y, dir + 2);
+    }
+    else{ 
+      upd(x, y, dir);
+      upd(x, y, dir + 1);
+      upd(x, y, dir + 2);
+      upd(x, y, dir + 3);
+    }
+
+    dfs(idx + 1);
   }
 }
 
 int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
-  cout.tie(0);
 
   cin >> n >> m;
 
-  int mn = 0;
-
-  for(int i=0;i<n;i++){
-    for(int j=0;j<m;j++){
-      cin >> board1[i][j];
-      if(board1[i][j] != 0 && board1[i][j] != 6) cctv.push_back({i,j});
-      if(board1[i][j] == 0) mn++;
-    }
-  }
-
-  int total = 1;
-  for(int i=0;i<cctv.size();i++){
-    total *= 4;
-  }
-
-  for(int tmp=0;tmp<total;tmp++){
-
-    for(int i=0;i<n;i++)
-      for(int j=0;j<m;j++)
-        board2[i][j] = board1[i][j];
-
-    int brute = tmp;
-    for(int i=0;i<cctv.size();i++){
-      int dir = brute % 4;
-      brute /= 4;
-      int x, y;
-      tie(x,y) = cctv[i];
-      if(board1[x][y] == 1){
-        upd(x,y,dir);
-      } else if(board1[x][y] == 2){
-        upd(x,y,dir);
-        upd(x,y,dir+2);
-      } else if(board1[x][y] == 3){
-        upd(x,y,dir);
-        upd(x,y,dir+1);
-      } else if(board1[x][y] == 4){
-        upd(x,y,dir);
-        upd(x,y,dir+1);
-        upd(x,y,dir+3);
-      } else if(board1[x][y] == 5){
-        upd(x,y,dir);
-        upd(x,y,dir+1);
-        upd(x,y,dir+2);
-        upd(x,y,dir+3);
-      }
-
-    int val = 0;
-    for(int i=0;i<n;i++){
-      for(int j=0;j<m;j++){
-        if(board2[i][j] == 0) val++;
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < m; j++){
+      cin >> a[i][j];
+      b[i][j] = a[i][j];
+      if(a[i][j] >= 1 && a[i][j] <= 5){
+        camera.push_back({i, j});
       }
     }
-    mn = min(mn, val);
-    }
   }
 
-cout << mn;
+  dfs(0);
 
+  cout << mn;
 }
