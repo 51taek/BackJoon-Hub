815338bb6ce6e9805052eee0b44285fd3253b2ec
wontaek oh <61085049+51taek@users.noreply.github.com>
2026-03-31 11:39:18 +0900

    [Gold V] Title: 선 긋기, Time: 348 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.cc
+++ 백준/Gold/2170. 선 긋기/선 긋기.cc
@@ -1,41 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n;
+vector<pair<int,int>> v;
+
 #define X first
 #define Y second
-
-int n, x, y;
-vector<pair<int, int>> v;
 
 int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
 
-  int ans = 0;
-
   cin >> n;
-  
+
   for(int i=0;i<n;i++){
+    int x, y;
     cin >> x >> y;
     v.push_back({x,y});
   }
 
   sort(v.begin(), v.end());
 
-  int st = v[0].X; int en = v[0].Y;
-
+  int st = v[0].X;
+  int en = v[0].Y;
+  long long ans = 0;
   for(int i=1;i<n;i++){
-    if(en<v[i].X){
+    if(en < v[i].X){
       ans += en - st;
       st = v[i].X;
       en = v[i].Y;
-    } else {
+    } else{
       en = max(en, v[i].Y);
     }
   }
-  ans += en - st;
-  
+
+  ans += en-st;
 
   cout << ans;
+  
 }
