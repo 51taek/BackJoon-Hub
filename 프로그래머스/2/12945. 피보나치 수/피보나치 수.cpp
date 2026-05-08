7f1e194d35c92ba490a6b736f8201d8b1655a383
wontaek oh <61085049+51taek@users.noreply.github.com>
2026-05-08 11:44:13 +0900

    [level 2] Title: 피보나치 수, Time: 0.40 ms, Memory: 4.05 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int dp[100005];
+
+int solution(int n) {
+    int answer = 0;
+    
+    dp[1] = 1;
+    dp[2] = 1;
+    for(int i=3;i<=n;i++){
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+    }
+    
+    return dp[n];
+}
